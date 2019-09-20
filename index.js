25333c4c8ffe36e9295c209437fccb69d1242c83
JINGYIDING <dingjy94@gmail.com>
2019-09-19 18:39:30 -0700

    random api and test finished

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ import axios from "axios";
 const url = "https://en.wikipedia.org/w/api.php";
 
 /**
- * Pre-process response, reject warning and error
- * @param {Object} res: response of wiki api
+ * @summary: Pre-process response, reject warning and error
+ * @param {Object} res - response of wiki api
  *
- * @return {Promise}
+ * @return {Promise} - response data
  */
 const wikiResponseProcess = (res) => {
   const data = res.data;
@@ -26,19 +26,26 @@ const wikiResponseProcess = (res) => {
   }
 
   return Promise.resolve(data);
-}
+};
 
-const onError = function (error) {
+/**
+ * @summary error handler
+ * @param {*} error
+ * @return {Promise} - reject promise, return parsed error message
+ */
+const onError = (error) => {
   console.error('Request Failed:', error);
 
   return Promise.reject(error.response || error.message || error);
-}
+};
 
 /**
+ * @summary search for page by title or content
+ * @param {string} value - Search for page titles or content matching this value.
+ * @param {Number} limit - How many total result pages
+ * @param {Number} page - The index of 'search' papge, eg. if page == 0, return the first limit number of page, if page == 2, return the second limit number of page
+ * @return {Promise} - result object { search: array of result pages, limit, page, total: total hit pages number }
  *
- * @param {*} value
- * @param {*} limit
- * @param {*} page
  */
 const search = (value, limit = 20, page = 0) => {
   if (!value) {
@@ -72,8 +79,35 @@ const search = (value, limit = 20, page = 0) => {
            .catch(onError);
 };
 
+const random = (limit = 20, rncontinue = null)=> {
+  const params = {
+    action: 'query',
+    format: 'json',
+    list: 'random',
+    rnlimit: limit,
+    rncontinue
+  };
+
+  const onSuccess = (data) => {
+    const result = {
+      random: data.query.random,
+      rncontinue: data.continue.rncontinue,
+      limit: limit
+    };
+
+    console.debug(`Get ${limit} random pages successfully!`);
+    return result;
+  };
+
+  return axios.get(url, { params })
+    .then(wikiResponseProcess)
+    .then(onSuccess)
+    .catch(onError);
+};
+
 const wiki = {
-  search
+  search,
+  random
 };
 
 export default wiki;
--- index.test.js
+++ index.test.js
@@ -23,3 +23,17 @@ describe('search', () => {
     return expect(wiki.search('code', 20, -10)).rejects.toEqual(expect.stringContaining('Error'));
   });
 });
+
+describe('random', () => {
+  test('call without limit should return 20 random pages', () => {
+    return wiki.random().then(data => {
+      expect(data.random.length).toBeLessThanOrEqual(20);
+      expect(data.limit).toBe(20);
+      expect(data).toHaveProperty('rncontinue');
+    });
+  });
+
+  test('Wrong limit value fails with warning', () => {
+    return expect(wiki.random(-20)).rejects.toEqual(expect.stringContaining('Warning'));
+  });
+});
